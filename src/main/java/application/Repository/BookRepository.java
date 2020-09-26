b590890bb18c54df3b09bc4908a3b3f565cbba96
Abhinavsds <49261415+Abhinavsds@users.noreply.github.com>
2020-09-26 14:22:41 +0530

    Added stored procedure in comment lines

--- src/main/java/application/Repository/BookRepository.java
+++ src/main/java/application/Repository/BookRepository.java
@@ -27,6 +27,15 @@ public interface BookRepository extends CrudRepository<Book, Integer> {
 	@Query(nativeQuery = true,value = "call exportToExcel(:Id)")   // @Procedure not work with return row  details have to this way
     List<Book> exportToExcel(@Param("Id")int Id);
 	
+	
+	/*
+	 * 
+	 * 
+	 * CREATE DEFINER=`root`@`localhost` PROCEDURE `exportToExcel`(id int)
+begin
+    select * from book ;
+     
+     end*/
 //	@Query(value="Select * FROM book where author= ?1",nativeQuery = true)  for native query
 	@Query("Select bk FROM book bk where author= ?1")   //using jpl query but for thtis we have to name entity aloso as book.
 	List<Book> findByAuthor(String author); 
